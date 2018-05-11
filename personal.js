87f358a5b4503713b57287ec138c44ae4f07ab81
Matt Kirk <matt.22222@hotmail.com>
2018-05-11 10:54:26 -0500

    personal first commit

---
+++ personal.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+
+    // Activate Carousel
+    $("#myCarousel").carousel();
+
+    // Enable Carousel Indicators
+    $(".item1").click(function(){
+        $("#myCarousel").carousel(0);
+    });
+    $(".item2").click(function(){
+        $("#myCarousel").carousel(1);
+    });
+
+    // Enable Carousel Controls
+    $(".left").click(function(){
+        $("#myCarousel").carousel("prev");
+    });
+    $(".right").click(function(){
+        $("#myCarousel").carousel("next");
+    });
+
+    $('a[href*="#"]')
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function(event) {
+            if (
+            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+            && 
+            location.hostname == this.hostname
+            ) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                event.preventDefault();
+                $('html, body').animate({
+                scrollTop: target.offset().top
+                }, 1000, function() {
+                var $target = $(target);
+                $target.focus();
+                if ($target.is(":focus")) { 
+                    return false;
+                } else {
+                    $target.attr('tabindex','-1'); 
+                    $target.focus();
+                };
+                });
+            }
+            }
+        });
+
+});
